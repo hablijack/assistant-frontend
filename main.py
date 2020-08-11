71465a730da24b924e93f2053ccb96e1e376a741
hablijack <christoph.habel@posteo.de>
2020-08-11 07:57:11 +0200

    mpg123 and https gets error - fix to curl and pipe

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 from flask_socketio import SocketIO
 from flask import Flask, request, render_template, Response, send_from_directory, jsonify
+import subprocess
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='threading')
+
+def background_thread(url):
+    try:
+        curl = subprocess.Popen(['curl', '--silent', url], stdout=subprocess.PIPE)
+        mpg = subprocess.Popen(['mpg123', '-q', '-a', 'hw:1,0', '-'], stdin=curl.stdout, stdout=subprocess.PIPE).wait()
+        #subprocess.Popen([ '/usr/bin/mpg123', '-a', 'hw:1,0', url ], stdout=subprocess.PIPE ).wait()
+    except:
+        print("nix fehler gut")
+    finally:
+        socketio.emit('change', 'waiting')
+
 
 @app.route('/assets/<path:path>')
 def send_assets(path):
     return send_from_directory('assets', path)
 
 @app.route('/screen')
-def sessions():
+def screen():
     return render_template('index.html')
 
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
-
-@socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
+@app.route('/change', methods=['POST'])
+def change():
+    print('received change event by api gateway')
+    topic = request.json['topic']
+    if topic == "news":
+        socketio.emit('change', 'news')
+        thread = socketio.start_background_task(background_thread, request.json['url'])
+    return jsonify({"state": "ok"})
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0')
